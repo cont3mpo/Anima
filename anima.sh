57180625f978399282f3232ae7c27495fd92cf27
cont3mpo <cont3mpo@gmail.com>
2015-09-30 21:53:59 -0300

    Update anima.sh

--- anima.sh
+++ anima.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 nom="`echo $1 | sed 's/\.[^.]*$//'`"
 if [ "$1" ] && [ "$2" ] && [ "$3" ]; then
-echo "Anima - Encodeando video a Gif..."
+echo "Encodeando video a Gif..."
 mkdir -p .caps
 ffmpeg -ss "$2" -i "$1" -vf scale=480:-1 -r 10 -t "$3" .caps/ima%04d.png 2> /tmp/anima-error.log
 convert .caps/ima*.png "$nom".gif 2> /tmp/anima-error.log
 rm -r .caps
-notify-send "Anima" "¡Gif listo!"
-echo "Anima - ¡Gif listo!"
+echo "¡Gif listo! ~ $nom.gif"
 else
-echo "Anima - Faltó video o tiempo o duración."
+echo "Ups, falta algo.
+Quizás ayude: ./anima.sh nombre.mp4 00:00:00 00"
 fi
